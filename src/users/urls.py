be6947297cbd9539dc894e00f6a70793288d2650
Gustavo Leme <gleme@nevada.unr.edu>
2020-05-14 01:27:10 -0300

    fixes broken tests

--- src/users/urls.py
+++ src/users/urls.py
@@ -7,5 +7,5 @@ app_name = 'users'
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
     path('login/', views.LoginUserView.as_view(), name='login'),
-    path('me/', views.ManageUserView.as_view(), name='profile')
+    path('me/', views.ManageUserView.as_view(), name='me')
 ]
