7a3b125c5ef024557f9130c8eff51bbcbdc7860c
Gustavo Leme <gleme@nevada.unr.edu>
2020-05-14 23:03:11 -0300

    adds ingredients creation functionality

--- src/users/urls.py
+++ src/users/urls.py
@@ -5,7 +5,7 @@ from users import views
 app_name = 'users'
 
 urlpatterns = [
-    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('', views.CreateUserView.as_view(), name='create'),
     path('login/', views.LoginUserView.as_view(), name='login'),
     path('me/', views.ManageUserView.as_view(), name='me')
 ]
