ea6e3670785ed1f6003125b127d0a7e4e0bc6c31
Gustavo Leme <gleme@nevada.unr.edu>
2020-05-16 19:19:14 -0300

    adds recipe model

--- src/core/models.py
+++ src/core/models.py
@@ -13,7 +13,7 @@ class UserManager(BaseUserManager):
             raise ValueError('Users must have an email address')
         new_user = self.model(email=self.normalize_email(email), **kwargs)
         new_user.set_password(password)
-        new_user.save(using=self.db)
+        new_user.save(using=self._db)
         return new_user
 
     def create_superuser(self, email, password):
@@ -21,7 +21,7 @@ class UserManager(BaseUserManager):
         new_user = self.create_user(email, password)
         new_user.is_staff = True
         new_user.is_superuser = True
-        new_user.save(using=self.db)
+        new_user.save(using=self._db)
         return new_user
 
 
@@ -58,3 +58,20 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    ingredients = models.ManyToManyField(Ingredient)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
